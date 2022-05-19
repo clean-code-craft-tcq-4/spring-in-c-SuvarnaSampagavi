ca8859b2553f3e982e89fc989e0f8233b4214b65
SuvarnaSampagavi <105294350+SuvarnaSampagavi@users.noreply.github.com>
2022-05-19 18:05:11 +0530

    Update stats.c
    Adding code to find avg, min and max in function "compute_statistics"

--- stats.c
+++ stats.c
@@ -5,6 +5,41 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    int index = 0;
+    float sum = 0;
+    float large, small;
+    large = small = numberset[0];
+
+    //Finding Average
+    for(i = 0; i < setlength; i++)
+    {
+        sum = sum + numberset[i];
+    }
+    s.average = sum/setlength;
+    
+    // Finding Max and Min
+    for(i = 1; i < setlength; i++)
+    {
+    	if(large < numberset[i])
+    	{
+    		large = numberset[i];
+    	}
+    	if(small > numberset[i])
+    	{
+    		small = numberset[i];
+    	}
+    }
+    s.max = large;
+    s.min = small;
+
+
+    if(numberset == 0)
+    {
+    	s.average = 0;
+    	s.min = 0;
+    	s.max = 0;
+    }  
 }
 
 int emailAlertCallCount = 0;
